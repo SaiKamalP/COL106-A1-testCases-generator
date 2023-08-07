b49c7d8901d899c13eadfddc805a2dfb6fa02d7f
logxdx <iamvarun158@gmail.com>
2023-08-07 14:48:03 +0530

    update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -3,8 +3,11 @@
 using namespace std;
  
 int main() {
+
+    int test_cases = 0; //change here
+    
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-    for(int i=1;i<=20;i++){
+    for(int i=1;i<=test_cases;i++){
         cout<<"checking test "<<i<<endl;
         string s="./main <./testcases/test-"+to_string(i)+".txt | diff ./testcases/result-test-"+to_string(i)+".txt -";
         system(&s[0]);
