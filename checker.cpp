9354991ae69fbdffd38d67063ebba2af4629f9b3
logxdx <iamvarun158@gmail.com>
2023-08-07 14:12:53 +0530

    windows support

--- checker.cpp
+++ checker.cpp
@@ -3,12 +3,19 @@
 using namespace std;
  
 int main() {
+    int test_cases = 0;  // change this here
+
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-    for(int i=1;i<=20;i++){
-        cout<<"checking test "<<i<<endl;
-        string s="./main <./testcases/test-"+to_string(i)+".txt | diff ./testcases/result-test-"+to_string(i)+".txt -";
+    system("mkdir output_generated");
+    for(int i=1;i<=test_cases;i++){
+        cout<<"generating output "<<i<<endl;
+        string s="main <testcases/test-"+to_string(i)+".txt> myoutput-test-"+to_string(i)+".txt";
         system(&s[0]);
     }
-    
+    for(int i=1;i<=test_cases;i++){
+        cout<<"checking test "<<i<<endl;
+        string s="fc /n /w myoutput-test-"+to_string(i)+".txt testcases/result-test-"+to_string(i)+".txt";
+        system(&s[0]);
+    }
     return 0;
 }
